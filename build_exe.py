81758b42c81199e8717ae30ff1dfadd93f7b30af
darkArp <22871633+darkarp@users.noreply.github.com>
2019-12-02 10:08:20 +0100

    Add files via upload

---
+++ build_exe.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""build_exe script."""
+
+from pexe3.build_exe import main
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3.3
+# -*- coding: utf-8 -*-
+"""setup script for py2exe.
+"""
+
+import os
+import sys
+
+if sys.version_info < (3, 3):
+    raise RuntimeError("This package requires Python 3.3 or later")
+
+############################################################################
+
+from setuptools import setup
+##from distutils.core import setup
+
+from pexe3.pexe3_distutils import Dist, Interpreter, BuildInterpreters
+
+############################################################################
+
+def _is_debug_build():
+    import imp
+    for ext, _, _ in imp.get_suffixes():
+        if ext == "_d.pyd":
+            return True
+    return False
+
+if _is_debug_build():
+    macros = [("PYTHONDLL", '\\"python%d%d_d.dll\\"' % sys.version_info[:2]),
+##              ("PYTHONCOM", '\\"pythoncom%d%d_d.dll\\"' % sys.version_info[:2]),
+              ("_CRT_SECURE_NO_WARNINGS", '1')]
+else:
+    macros = [("PYTHONDLL", '\\"python%d%d.dll\\"' % sys.version_info[:2]),
+##              ("PYTHONCOM", '\\"pythoncom%d%d.dll\\"' % sys.version_info[:2]),
+              ("_CRT_SECURE_NO_WARNINGS", '1'),]
+
+macros.append(("Py_BUILD_CORE", '1'))
+
+extra_compile_args = []
+extra_link_args = []
+
+extra_compile_args.append("-IC:\\Program Files\\Microsoft SDKs\\Windows\\v7.0\\Include")
+extra_compile_args.append("-IC:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\include")
+extra_compile_args.append("-IC:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.10586.0\\ucrt")
+
+if 0:
+    # enable this to debug a release build
+    extra_compile_args.append("/Od")
+    extra_compile_args.append("/Z7")
+    extra_link_args.append("/DEBUG")
+    macros.append(("VERBOSE", "1"))
+
+run_ctypes_dll = Interpreter("pexe3.run_ctypes_dll",
+                             ["source/run_ctypes_dll.c",
+                              "source/start.c",
+                              "source/icon.rc",
+
+                              "source/MemoryModule.c",
+                              "source/MyLoadLibrary.c",
+                              "source/_memimporter.c",
+                              "source/actctx.c",
+
+                              "source/python-dynload.c",
+                              ],
+                             libraries=["user32", "shell32"],
+                             export_symbols=["DllCanUnloadNow,PRIVATE",
+                                             "DllGetClassObject,PRIVATE",
+                                             "DllRegisterServer,PRIVATE",
+                                             "DllUnregisterServer,PRIVATE",
+                                             ],
+                             target_desc = "shared_library",
+                             define_macros=macros,
+                             extra_compile_args=extra_compile_args,
+                             extra_link_args=extra_link_args + ["/DLL"],
+                             )
+
+run = Interpreter("pexe3.run",
+                  ["source/run.c",
+                   "source/start.c",
+                   "source/icon.rc",
+
+                   "source/MemoryModule.c",
+                   "source/MyLoadLibrary.c",
+                   "source/_memimporter.c",
+                   "source/actctx.c",
+
+                   "source/python-dynload.c",
+                   ],
+                  libraries=["user32", "shell32"],
+                  define_macros=macros,
+                  extra_compile_args=extra_compile_args,
+                  extra_link_args=extra_link_args,
+                  )
+
+run_w = Interpreter("pexe3.run_w",
+                    ["source/run_w.c",
+                     "source/start.c",
+                     "source/icon.rc",
+
+                     "source/MemoryModule.c",
+                     "source/MyLoadLibrary.c",
+                     "source/_memimporter.c",
+                     "source/actctx.c",
+
+                     "source/python-dynload.c",
+                     ],
+                    libraries=["user32", "shell32"],
+                    define_macros=macros,
+                    extra_compile_args=extra_compile_args,
+                    extra_link_args=extra_link_args,
+                    )
+
+
+resource_dll = Interpreter("pexe3.resources",
+                           ["source/dll.c",
+                            "source/icon.rc"],
+                           target_desc = "shared_library",
+                           extra_link_args=["/DLL"],
+                           )
+
+interpreters = [run, run_w, resource_dll,
+                run_ctypes_dll]
+
+
+if __name__ == "__main__":
+    import pexe3
+
+    cmdclass = {'build_interpreters': BuildInterpreters}
+
+    setup(name="pexe3",
+          version=pexe3.__version__,
+          description="Python to Executable",
+          author="darkArp",
+          author_email="marionascimento@itsec.bz",
+          license="MIT/X11",
+          install_requires=["cachetools", "pefile"],
+          platforms="Windows",
+          download_url="http://sourceforge.net/project/showfiles.php?group_id=15583",
+
+          classifiers=[
+              "Development Status :: 4 - Beta",
+              "Environment :: Console",
+              "License :: OSI Approved :: MIT License",
+              "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+              "Operating System :: Microsoft :: Windows",
+              "Programming Language :: C",
+              "Programming Language :: Python :: 3",
+              "Programming Language :: Python :: 3.7",
+              "Programming Language :: Python :: Implementation :: CPython",
+              "Topic :: Software Development",
+              "Topic :: Software Development :: Libraries",
+              "Topic :: Software Development :: Libraries :: Python Modules",
+              "Topic :: System :: Software Distribution",
+              "Topic :: Utilities",
+              ],
+
+          distclass = Dist,
+          cmdclass = cmdclass,
+          scripts = ["build_exe.py"],
+          entry_points = {
+              'console_scripts': ['build_exe = pexe3.build_exe:main'],
+              },
+          interpreters = interpreters,
+          py_modules=['zipextimporter'],
+          packages=['pexe3'],
+          )
